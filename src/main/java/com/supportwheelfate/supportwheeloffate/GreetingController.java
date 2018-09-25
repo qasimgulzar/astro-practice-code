f93991a265e9b440a1b46d66746ea75b6567fa58
qkhokhar <qkhokhar@sparkcognition.com>
2018-09-25 17:02:00 +0500

    [INIT] up and running.

---
+++ src/main/java/com/supportwheelfate/supportwheeloffate/GreetingController.java
@@ -0,0 +1,13 @@
+package com.supportwheelfate.supportwheeloffate;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ src/main/java/com/supportwheelfate/supportwheeloffate/SupportWheelOfFateApplication.java
@@ -0,0 +1,13 @@
+package com.supportwheelfate.supportwheeloffate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportWheelOfFateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(SupportWheelOfFateApplication.class);
+		application.run(args);
+	}
+}
