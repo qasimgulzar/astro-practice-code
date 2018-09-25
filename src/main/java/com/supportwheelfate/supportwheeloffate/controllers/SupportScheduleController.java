a60355f595182a161d439fac01ba9df265f952b5
qkhokhar <qkhokhar@sparkcognition.com>
2018-09-25 17:05:37 +0500

    [NEW] schedule controller.

--- src/main/java/com/supportwheelfate/supportwheeloffate/controllers/SupportScheduleController.java
+++ src/main/java/com/supportwheelfate/supportwheeloffate/controllers/SupportScheduleController.java
@@ -1,11 +1,11 @@
-package com.supportwheelfate.supportwheeloffate;
+package com.supportwheelfate.supportwheeloffate.controllers;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class SupportScheduleController {
     @RequestMapping("/hello/{name}")
     String hello(@PathVariable String name) {
         return "Hello, " + name + "!";
