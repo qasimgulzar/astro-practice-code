5f40838d01dd4d467bf656121f4ec361259c6435
qkhokhar <qkhokhar@sparkcognition.com>
2018-09-26 13:06:48 +0500

    [DOCUMENTATION]

--- src/main/java/com/supportwheelfate/supportwheeloffate/services/IScheduleService.java
+++ src/main/java/com/supportwheelfate/supportwheeloffate/services/IScheduleService.java
@@ -6,5 +6,11 @@ import java.util.Date;
 import java.util.List;
 
 public interface IScheduleService {
+
+    /**
+     *  @param persons list of persons.
+     *  @param days total number of days to generate schedule.
+     *  @param startDate date from where we have to start schedule.
+     * */
     List<Schedule> schedule(String[] persons, int days, Date startDate);
 }
